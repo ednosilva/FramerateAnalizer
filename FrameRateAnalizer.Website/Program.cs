4ccdb256181879a70859b85ca33531e5acd8f2cf
Victor Henrique Silva Galvão <henrictor@hotmail.com>
2026-01-16 15:40:16 -0300

    testes com framework AntDesign

--- FrameRateAnalizer.Website/Program.cs
+++ FrameRateAnalizer.Website/Program.cs
@@ -6,6 +6,7 @@ using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddMudServices();
+builder.Services.AddAntDesign();
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
